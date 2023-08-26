b40f84098f3c2a3d18e3f6f0aff2c126f884ef66
Guilherme Pacianotto <gpacianotto@gmail.com>
2023-08-26 16:45:50 -0300

    terminamos a interpretação do ipv6

--- IPv6Interpreter.py
+++ IPv6Interpreter.py
@@ -12,14 +12,38 @@ class IPv6Interpreter:
         result = {
             "hexa_values": []
         }
+        zero = [
+            HexNumber.HexNumber([False, False, False, False]),
+            HexNumber.HexNumber([False, False, False, False]),
+            HexNumber.HexNumber([False, False, False, False]),
+            HexNumber.HexNumber([False, False, False, False])
+        ]
+        buffer = []
+        ipv6 = []
         for char in raw:
             char_upper = char.upper()
+            
             if char_upper in self.hex_map.values():
+                print("char lido: " + char_upper)
                 binary_array = NumberProcessor.NumberProcessor().hexa_to_binary_array(char_upper)
+                
                 hexa_number = HexNumber.HexNumber(binary_array)
-                result["hexa_values"].append(hexa_number)
-            # elif char_upper == ":":
-
+                # result["hexa_values"].append(hexa_number)
+                buffer.append(hexa_number)
+                print("hexa_number:  " + str(hexa_number))
+            elif char_upper == ":":
+                while len(buffer) < 4:
+                    buffer.insert(0, HexNumber.HexNumber([False, False, False, False]))
+                ipv6.append(buffer)
+                buffer = []
+            elif char_upper == "/":
+                print("PEGOU A BARRA")
+                while len(buffer) < 4:
+                    buffer.insert(0, HexNumber.HexNumber([False, False, False, False]))
+                ipv6.append(buffer)
+                buffer = []
             else:
-                print("X")
-        print(result)
+                print("DEU ALGUM ERRO CABULOSO")
+        while len(ipv6) < 8:
+            ipv6.append(zero)
+        print(ipv6)
--- teste2.py
+++ teste2.py
@@ -14,3 +14,4 @@ import IPv6Interpreter
 # print(NumberProcessor.NumberProcessor().decimal_to_binary_array(25))
 
 print(IPv6Interpreter.IPv6Interpreter("2001:db8::/32").process_address())
+# print(IPv6Interpreter.IPv6Interpreter("2001:DB8:0:BEBA::C0:ca/32").process_address())
